d63485cbcc09ddd2ca316164425881b4144c398e
Tomas <tomasholderness@gmail.com>
2018-04-17 10:55:52 -0400

    testing param parse

--- src/functions/notify/index.js
+++ src/functions/notify/index.js
@@ -4,13 +4,17 @@ import response from '../../lib/response';
 export default (event, context, callback) => {
   // Log statements
   console.log('\n\nLoading Notify handler\n\n');
-  console.log('Incoming body: ' + JSON.stringify(event.body));
+  console.log('Incoming body: ' + event.body);
+
+  // Parse params
+  const params = JSON.parse(event.body);
 
   // Create notify object
   const notify = new Notify(process.env.AWS_REGION,
     process.env.AWS_SNS_ACCOUNT_ID);
 
-  notify.send(event.body)
+  // Request notification
+  notify.send(params)
     .then((result) => {
       callback(response(200, 'success!'));
     }).catch((err) => {
